dbd6ab71001b552c386397e682461b01c9f7b550
Ahmet Serkan Goktas <ahmet.goktas@piworks.net>
2019-08-08 10:40:48 +0300

    done

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -1,30 +1,57 @@
 
 
-describe('Product Application', ()=>{
+describe('Product Application', () => {
 
-    it('should redirect index.html/errorpath to index.html#!/', ()=>{
+    it('should redirect index.html/errorpath to index.html#!/', () => {
         browser.get('index.html');
         expect(browser.getCurrentUrl()).not.toContain('index.html#!/errorpath');
     });
 
 
-    describe('The opening page is the product-list page. This page consist of a table of products and buttons to make actions about them.', ()=>{
+    describe('The opening page is the product-list page. This page consist of a table of products and buttons to make actions about them.', () => {
 
-        beforeEach(()=>{
+        beforeEach(() => {
             browser.get('index.html#!/');
         });
 
-        it('should display all elements', ()=>{
+        it('should display all elements', () => {
             expect(element(by.binding("product.ID")).getText()).toBe("13")
         });
 
-        it('buttons should redirect to correct urls', () => {
-            let row, add, edit, det, del;
-            
-            row=element.all(by.tagName('tbody')).then( row => {
-                console.log(row[0].getText());
+        it('should add items when navigating to add-product page', async () => {
+            const theProduct = { ID: 0, Name: "Veli", Detail: "Very good boy!", NumberStock: 4 };
+
+            await browser.get('index.html#!/');
+
+            element(by.css('.btn-primary')).click();
+
+            await element(by.css('input[name="Name"]')).sendKeys(theProduct.Name);
+            element(by.css('input[name="Detail"]')).sendKeys(theProduct.Detail);
+            element(by.css('input[name="Number"]')).sendKeys(theProduct.NumberStock);
+
+            element(by.css('button')).click();
+
+            expect(await element(by.css('#response')).getText()).toBe("Success!");
+
+            await browser.get('index.html#!/');
+
+            expect(element.all(by.binding("product.Name")).last().getText()).toBe(theProduct.Name);
+            expect(element.all(by.binding("product.NumberStock")).last().getText()).toBe(theProduct.NumberStock.toString());
+        });
+
+        xit('should delete items properly', async () => {
+            let theProduct;
+
+            await browser.get('index.html#!/');
+
+            var row = element.all(by.repeater('product in $ctrl.products')).first();
+            var cells = row.all(by.tagName('td'));
+
+            var cellTexts = cells.map(function (elm) {
+                return elm.getText();
             });
-            
+
+            console.log(cellTexts);
         });
     });
 
